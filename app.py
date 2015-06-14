bdc345974c2bff4516cab021fdaa89e6efe1d4d0
Andreas 'count' Kotes <count@flatline.de>
2015-06-14 04:41:44 +0200

    some SSL + LDAP integration

--- app.py
+++ app.py
@@ -4,19 +4,68 @@ from datetime import datetime, timedelta
 from flask import Flask
 from flask import session, request
 from flask import render_template, redirect, jsonify
+from flask_ldapconn import LDAPConn
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import gen_salt
 from flask_oauthlib.provider import OAuth2Provider
+from pprint import pprint
+import json
+import ssl
+import os
 
+# basic configuration
+config = {
+    'name':  'cteward-auth',
+    'debug': False,
+    'appconfig':   {
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
+    },
+    'app': {
+        'host': '0.0.0.0'
+    }
+}
 
-app = Flask(__name__, template_folder='templates')
-app.debug = True
-app.secret_key = 'secret'
-app.config.update({
-    'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
-})
+if 'CTEWARD_AUTH_CONFIG' in os.environ:
+    configfile = os.environ['CTEWARD_AUTH_CONFIG']
+else:
+    configfile = '/etc/cteward/auth.json'
+
+with open(configfile) as json_file:
+    config.update(json.load(json_file))
+
+# additional configuration
+if 'ssl' in config:
+    if not 'PREFERRED_URL_SCHEME' in config['appconfig']:
+        config['appconfig']['PREFERRED_URL_SCHEME'] = 'https'
+    if not 'ciphers' in config['ssl']:
+        config['ssl']['ciphers'] = 'EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH'
+    if not 'dh_params' in config['ssl']:
+        config['ssl']['dh_params'] = '/etc/cteward/dhparams.pem'
+
+app = Flask(config['name'], template_folder='templates')
+app.debug = config['debug']
+app.secret_key = config['secret']
+app.config.update(config['appconfig'])
+
+if 'ldapconfig' in config:
+    app.config.update(config['ldapconfig'])
+    if not 'LDAP_VERIFY_SSL' in config['ldapconfig'] or config['ldapconfig']['LDAP_VERIFY_SSL']:
+        app.config.update({'LDAP_REQUIRE_CERT': ssl.CERT_REQUIRED})
+    ldap = LDAPConn(app)
+
 db = SQLAlchemy(app)
 oauth = OAuth2Provider(app)
+
+if 'ssl' in config:
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    ssl_context.load_cert_chain(config['ssl']['certfile'],config['ssl']['keyfile'])
+    ssl_context.set_ciphers(config['ssl']['ciphers'])
+    ssl_context.options |= ssl.OP_CIPHER_SERVER_PREFERENCE
+    ssl_context.load_dh_params(config['ssl']['dh_params'])
+    # FIXME: missing OCSP stapling
+    # FIXME: disable SSL session tickets
+else:
+    ssl_context = None
 
 
 class User(db.Model):
@@ -122,6 +171,21 @@ def current_user():
         return User.query.get(uid)
     return None
 
+
+#### TODO: integrated this properly
+username = 'username'
+password = 'password'
+attribute = 'uid'
+basedn = 'ou=crew,dc=c-base,dc=org'
+search_filter = ('(memberOf=cn=crew,ou=groups,dc=c-base,dc=org)')
+
+with app.app_context():
+    retval = ldap.authenticate(username, password, attribute, basedn, search_filter)
+    if retval:
+        print('Welcome %s.' % username)
+    else:
+        print('Auth failed.')
+####
 
 @app.route('/', methods=('GET', 'POST'))
 def home():
@@ -254,7 +318,14 @@ def me():
     user = request.oauth.user
     return jsonify(username=user.username)
 
+def add_common_response_headers(response):
+    # security headers
+    response.headers.add('Strict-Transport-Security','max-age=63072000; includeSubdomains; preload')
+    response.headers.add('X-Frame-Options','DENY')
+    response.headers.add('X-Content-Type-Options','nosniff')
+    return response
 
 if __name__ == '__main__':
     db.create_all()
-    app.run()
+    app.after_request(add_common_response_headers)
+    app.run(ssl_context=ssl_context, **config['app'])
